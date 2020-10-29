8816a1c98973e16d80582427336745c3637600ce
Eric Park <epark0995@gmail.com>
2020-10-29 12:08:19 -0700

    redo home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,10 +13,10 @@ const Home = () => {
                         <h1>About Me</h1>
                         <p>
                             lorem ipsum 
-                            <br/>
+                            {/* <br/>
                             <p><Button className='col-4' color="success"><a href="https://docs.google.com/document/d/1fhR-W9pTb3rxZCmACeilp7PmBP4LjCNYnFZe8KachOY/edit?usp=sharing" className="btn btn-success" target='_blank'> Online Resume</a></Button></p>
                             <p><Button className='col-4' color ='info'><a href="https://www.linkedin.com/in/eric-park-7122821b6/" className="btn btn-info" target='_blank'>Linkedin</a></Button></p>
-                            <p><Button className='col-4' color ='secondary'><a href="https://github.com/E-Park8" className="btn btn-secondary" target='_blank'>Github Profile</a></Button> </p>
+                            <p><Button className='col-4' color ='secondary'><a href="https://github.com/E-Park8" className="btn btn-secondary" target='_blank'>Github Profile</a></Button> </p> */}
                         </p>
 
                     </section>
