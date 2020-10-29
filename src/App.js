522cd4f8f3075b9b51e846b0c7663b21585b7fdb
Eric Park <epark0995@gmail.com>
2020-10-29 12:07:35 -0700

    fixed contact page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
 
+} from 'reactstrap'
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   return (
-   <>
-   <h1>Hello</h1>
-   </>
+    <Router>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink><Link to='/ericparkportfolio/'>Home</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to='/ericparkportfolio/portfolio'>Portfolio</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to='/ericparkportfolio/contact'>Contact</Link></NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/ericparkportfolio" component={Home} />
+          <Route path="/ericparkportfolio/portfolio" component={Portfolio} />
+          <Route path="/ericparkportfolio/contact" component={Contact} />
+        </Switch>
+      </div>
+
+      <footer className="fixed-bottom">     
+    
+      <Navbar color="dark" light expand="md">
+
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                
+              </NavItem>
+            </Nav>
+        </Navbar>
+
+      </footer>
+
+    </Router>
   )
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../../style.css'
+import { Button } from  'reactstrap'
+
+const Home = () => {
+  return (
+      <>
+    <h2 className='myName'>Eric Park</h2>
+    <h5 className='jobTitle'>Full Stack Web Developer</h5>
+    <hr/>
+    <div>
+    <section className="col-12">
+                        <h1>About Me</h1>
+                        <p>
+                            lorem ipsum 
+                            <br/>
+                            <p><Button className='col-4' color="success"><a href="https://docs.google.com/document/d/1fhR-W9pTb3rxZCmACeilp7PmBP4LjCNYnFZe8KachOY/edit?usp=sharing" className="btn btn-success" target='_blank'> Online Resume</a></Button></p>
+                            <p><Button className='col-4' color ='info'><a href="https://www.linkedin.com/in/eric-park-7122821b6/" className="btn btn-info" target='_blank'>Linkedin</a></Button></p>
+                            <p><Button className='col-4' color ='secondary'><a href="https://github.com/E-Park8" className="btn btn-secondary" target='_blank'>Github Profile</a></Button> </p>
+                        </p>
+
+                    </section>
+    </div>
+    </>
+  )
+}
+
+export default Home
