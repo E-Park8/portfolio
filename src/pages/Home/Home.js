786310bce5a469c81a8fc02ceea62fc4ce8cf482
Eric Park <epark0995@gmail.com>
2020-11-25 11:09:28 -0800

    notes for changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -29,6 +29,7 @@ const Home = () => {
             skillsFront={'HTML5, CSS3, Javascript, jQuery, React.js, Handlebars.js, AJAX, REST API'}
             skillsBack={'Node.js, Python3, Express.js, Axios, Sequelize, Mongoose'}
             skillsData={'MySQL, MongoDB'}
+            // Change the resume once it is updated
             resume={'https://docs.google.com/document/d/1fhR-W9pTb3rxZCmACeilp7PmBP4LjCNYnFZe8KachOY/edit?usp=sharing'}
             />          
           </p>
